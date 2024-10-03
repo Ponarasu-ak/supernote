a2595716ee6ba82330119dca214714c3c63dca2e
Ponarasu-ak <ponarasu.ak@gmail.com>
2024-10-03 11:07:15 +0530

    PopUP

--- my-app/src/components/Popup/index.jsx
+++ my-app/src/components/Popup/index.jsx
@@ -1,18 +1,52 @@
 import React from "react";
+import Radio from "../Radio";
 
-function Popup() {
-  return <div className="w-full h-screen box-border backdrop-blur flex justify-center items-center ">
+function Popup({ setisopen, setdata, data }) {
+  console.log(data,'lllllllllll');
 
-
-  <div className="w-[90%] h-[60%] md:w-[60%] md:h-[40%] lg:w-[45%] lg:h-[50%] border border-red-500">
-
-  </div>
-
+  function handleadd() {
+    
+  }
   
+  return (
+    <div className="w-full h-screen box-border backdrop-blur flex justify-center items-center ">
+      <div className="  border rounded-md border-grey-500 ">
+        <div className="bg-white p-4 px-3 w-full h-full flex flex-col   justify-center items-center">
+          <div className="w-64 md:w-96 flex justify-between pb-2 ">
+            <button onClick={() => setisopen(false)} className=" px-3 py-1  ">
+              Cancel
+            </button>
+            <button onClick={handleadd} className="border text-white  bg-gray-600 rounded px-10 py-1">
+              Add
+            </button>
+          </div>
 
+          <div>
+            <input
+              className="bg-blue-100 w-64 md:w-96  h-12 p-2 outline-none broder mb-3"
+              type="text"
+              placeholder="Title..."
+              value={data.title}
+              onChange={(e)=>setdata({...data,title:e.target.value})}
+            />
+          </div>
 
-
-  </div>;
+          <div>
+            <textarea
+              className="bg-blue-100 w-64 md:w-96 h-40 p-2 outline-none "
+              type="text"
+              placeholder="Description..."
+              value={data.description}
+              onChange={(e)=>setdata({...data,description:e.target.value})}
+            />
+          </div>
+          <div>
+            <Radio setdata={setdata} data={data} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Popup;
--- my-app/src/components/index.jsx
+++ my-app/src/components/index.jsx
@@ -1,1 +1,2 @@
 export{default as Popup} from './Popup/index'
+export{default as Radio} from './Radio/index'
